536fa47be5848a7b23d8f0691e13ae5786ca5625
Brandon <brandon@reddirtwebdesign.com>
2018-04-06 08:53:21 +0000

    Schema Files

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,23 +1,2 @@
-import { createApolloServer } from 'meteor/apollo'
-import { makeExecutableSchema } from 'graphql-tools'
+import './registerApi'
 
-const typeDefs = `
-  type Query {
-   hi: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hi() {
-      return 'Hello Level Up!'
-    }  
-  }
-}
-
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
-})
-
-createApolloServer({ schema })
