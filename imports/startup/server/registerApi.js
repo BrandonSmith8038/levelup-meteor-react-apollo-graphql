bf6827c7e5ef6c19df9b607d43e3d86204d03bef
Brandon <brandon@reddirtwebdesign.com>
2018-04-07 11:49:40 +0000

    Extending Query Types

--- imports/startup/server/registerApi.js
+++ imports/startup/server/registerApi.js
@@ -6,27 +6,13 @@ import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql'
 import ResolutionResolvers from '../../api/resolutions/resolvers'
 import UsersSchema from '../../api/users/Users.graphql'
 import UsersResolvers from '../../api/users/resolvers'
-/////
-const testSchema  = `
-  type Query {
-   hi: String
-   resolutions: [Resolution]
-   user: User
-  }
-`
+/////afdsa
 
-const typeDefs = [testSchema, ResolutionsSchema, UsersSchema];
+const typeDefs = [ResolutionsSchema, UsersSchema];
 
-const testResolver = {
-  Query: {
-    hi() {
-      return 'Hello Level Up!'
-    }
-  }
-}
 
 const resolvers = merge(
-  testResolver, ResolutionResolvers, UsersResolvers
+  ResolutionResolvers, UsersResolvers
 )
 
 const schema = makeExecutableSchema({
